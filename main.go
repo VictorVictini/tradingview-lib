61b91256b8e3fd409533ccea6b9606e303ff0451
mohfez <150836596+mohfez@users.noreply.github.com>
2024-09-09 14:57:58 +0100

    fixed some bugs, added switch timezone req msg, added custom session type param when loading history

--- main.go
+++ main.go
@@ -10,20 +10,23 @@ func main() {
 		fmt.Println("Error: ", err)
 	} else {
 		//
-		//GetHistory("FOREXCOM:GBPJPY", "240")
-		//GetHistory("BATS:LLY", "240")
+		//GetHistory("FOREXCOM:GBPJPY", "240", "regular")
+		//GetHistory("BATS:LLY", "240", "regular")
 
-		fmt.Println("getting history:")
+		//fmt.Println("getting history:")
 		//AddRealtimeSymbols([]string{"FOREXCOM:GBPJPY"})
-		GetHistory("FOREXCOM:GBPJPY", "240")
+		//AddRealtimeSymbols([]string{"FOREXCOM:EURJPY"})
+		//GetHistory("FOREXCOM:GBPJPY", "240", "regular")
+		//SwitchTimezone("Europe/London")
 
-		fmt.Println("getting more candles:")
-		RequestMoreData(5)
-		//GetHistory("FOREXCOM:GBPUSD", "240")
+		//fmt.Println("getting more candles:")
+		//RequestMoreData(5)
+		//GetHistory("FOREXCOM:GBPUSD", "240", "regular")
 
 		//time.Sleep(2 * time.Second)
-		//GetHistory("FOREXCOM:GBPJPY", "240")
-		fmt.Println("finish")
+		//GetHistory("FOREXCOM:GBPJPY", "240", "regular")
+		//RemoveRealtimeSymbols([]string{"FOREXCOM:EURJPY"})
+		//fmt.Println("finish")
 		select {} // block program
 	}
 }
--- tv-history.go
+++ tv-history.go
@@ -8,24 +8,24 @@ var symbolCounter int = 0
 var seriesCounter int = 0
 var seriesCreated bool = false
 
-var maxHistoryCandles int = 10 // TODO make this changeable at start?
+var initHistoryCandles int = 10 // amount of candles to load at the start, then RequestMoreData can load more
 
 var resolvedSymbols map[string]string = make(map[string]string)
 var seriesMap map[string]string = make(map[string]string)
 
-func resolveSymbol(symbol string) error { // generalised params in sendMessage
-    if _, exists := resolvedSymbols[symbol]; exists {
-        return nil
-    }
+func resolveSymbol(symbol string, sessionType string) error { // session type can be either "regular" or "extended"
+	if _, exists := resolvedSymbols[symbol]; exists {
+		return nil
+	}
 
 	symbolCounter++
-    id := "symbol_" + strconv.Itoa(symbolCounter) //symbol id
+	id := "symbol_" + strconv.Itoa(symbolCounter) //symbol id
 
-    err := sendMessage("resolve_symbol", []interface{} {csToken, id, "={\"symbol\":\"" + symbol + "\",\"adjustment\":\"splits\",\"session\":\"regular\"}"});
-    if err != nil {
-        return err
-    }
+	err := sendMessage("resolve_symbol", []interface{}{csToken, id, "={\"symbol\":\"" + symbol + "\",\"adjustment\":\"splits\",\"session\":\"" + sessionType + "\"}"})
+	if err != nil {
+		return err
+	}
 
-    resolvedSymbols[symbol] = id
-    return nil
+	resolvedSymbols[symbol] = id
+	return nil
 }
